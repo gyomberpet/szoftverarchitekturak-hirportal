dcc534b6b292cb35c4078e03c48f4d77f35d4879
Benedek Sagi <0322sagibenedek@gmail.com>
2023-10-29 15:51:26 +0100

    Added the authentication

---
+++ NewsPortal.WebAppApi/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NewsPortal.WebAppApi.Data;
+using NewsPortal.WebAppApi.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NewsPortal.WebAppApi.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AuthController(DataContext context) {
+            _context = context;
+        }
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody]User user)
+        {
+            if(user == null)
+            {
+                return BadRequest("Bad client request");
+            }
+            foreach(var userItem in _context.Users) {
+                if(user.UserName == userItem.UserName && user.Password == userItem.Password)
+                {
+                    SymmetricSecurityKey secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                    SigningCredentials signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                    JwtSecurityToken tokeOptions = new JwtSecurityToken(
+                            issuer: "https://localhost:7021",
+                            audience: "https://localhost:7021",
+                            claims: new List<Claim>(),
+                            expires: DateTime.Now.AddMinutes(5),
+                            signingCredentials: signingCredentials
+                        );
+
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                    return Ok( new { Token = tokenString });
+                }              
+            }
+            return Unauthorized();
+        }
+    }
+}
+
--- NewsPortal.WebAppApi/Program.cs
+++ NewsPortal.WebAppApi/Program.cs
@@ -1,7 +1,10 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using NewsPortal.WebAppApi.Data;
 using NewsPortal.WebAppApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(
+    option =>
+        {
+            option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }
+    )
+    .AddJwtBearer(
+        options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = "https://localhost:7021",
+                ValidAudience = "https://localhost:7021",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+
+            };
+        }
+    );
 builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
@@ -17,6 +44,9 @@ var app = builder.Build();
 //Dataseeding start
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
+
+
+
 var dbContext = services.GetRequiredService<DataContext>();
 dbContext.Database.EnsureCreated();
 
@@ -24,12 +54,23 @@ if (!dbContext.News.Any())
 {
     var Newses = new List<News>
         {
-            new News { Title = "DummyNews 1", Subtitle = "Something1", Content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", IsTrending=false },
-            new News { Title = "DummyNews 2", Subtitle = "Something2", Content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", IsTrending=false },
-            new News { Title = "DummyNews 3", Subtitle = "Something3", Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", IsTrending = true},
+            new News {Id = "1", Title = "DummyNews 1", Subtitle = "Something1", Content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", IsTrending=false },
+            new News {Id = "2", Title = "DummyNews 2", Subtitle = "Something2", Content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", IsTrending=false },
+            new News {Id = "3", Title = "DummyNews 3", Subtitle = "Something3", Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", IsTrending = true},
         };
 
     dbContext.News.AddRange(Newses);
+    dbContext.SaveChanges();
+}
+if (!dbContext.Users.Any())
+{
+    var Users = new List<User>
+        {
+            new User {Id = "1",UserName = "gipszjakab", EmailAddress = "admin@kft.hu", Password = "1234",  IsAdmin = true },
+            new User {Id = "2",UserName = "shrek", EmailAddress = "onion@nasa.gov", Password = "1234",  IsAdmin = false }
+        };
+
+    dbContext.Users.AddRange(Users);
     dbContext.SaveChanges();
 }
 //Dataseeding end
@@ -41,6 +82,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
