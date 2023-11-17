6a28f3c6d26296a3cbbbd51dee41ee6b716c14fc
DQP <dauquangphong2000@gmail.com>
2023-11-17 21:29:06 +0100

    user_table
    user_table created - promote button is not working yet

---
+++ NewsPortal.WebAppUI/src/app/components/user-table-page/user-table-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UsersService } from 'src/app/service/users.service'; 
+
+@Component({
+  selector: 'app-user-table-page',
+  templateUrl: './user-table-page.component.html',
+  styleUrls: ['./user-table-page.component.css']
+})
+
+export class UserTablePageComponent implements OnInit {
+  users: User[];
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.usersService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  promoteUser(user: User): void {
+    this.usersService.promoteUser(user.id, !user.isAdmin).subscribe(promotedUser => {
+      user.isAdmin = promotedUser.isAdmin;
+    });
+  }
+}
